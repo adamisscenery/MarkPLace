d46bcf4e39280cf126763155632473e9be418287
adamisscenery <kaurimdu@gmail.com>
2020-09-14 16:49:56 +0600

    start

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    $(".product__sell-rate-star").rateYo({
+        rating: 4.5,
+        starWidth: "14px",
+        readOnly: true,
+    });
+
+    var mixer = mixitup('.sale__wrapper');
+
+});
