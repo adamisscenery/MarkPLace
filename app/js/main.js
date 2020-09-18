5dbf95f3d9767171c01ce48ffa0763adfb3fa5de
adamisscenery <kaurimdu@gmail.com>
2020-09-18 20:42:40 +0600

    added half-page

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,22 @@
 $(function(){
 
-    $(".product__sell-rate-star").rateYo({
+    $(".rate-star").rateYo({
         rating: 4.5,
         starWidth: "14px",
         readOnly: true,
     });
 
-    var mixer = mixitup('.sale__wrapper');
+    $('.big-slider__wrapper').slick({
+      arrows: true,
+      infinite: true,
+      prevArrow: '<button class="big-slide__slick-arrow big-slide__slick-prev"><span class="lnr lnr-chevron-left big-slide__arrow"></span></button>',
+      nextArrow: '<button class="big-slide__slick-arrow big-slide__slick-next"><span class="lnr lnr-chevron-right big-slide__arrow"></span></button>',
+    });
+
+    $('.category__inner-block-filter').on('click', function(){
+      $('.category__inner-block-filter-hover').slideToggle();
+    });
+
+    var mixer = mixitup('.category__wrapper-block');
 
 });
