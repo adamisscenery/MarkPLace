ef998e548c210da5cc20b1914f1b23d8e3fc8b63
adamisscenery <kaurimdu@gmail.com>
2020-09-21 19:36:18 +0600

    fixed html/scss code

--- app/js/main.js
+++ app/js/main.js
@@ -6,17 +6,17 @@ $(function(){
         readOnly: true,
     });
 
-    $('.big-slider__wrapper').slick({
+    $('.featured-product__wrapper').slick({
       arrows: true,
       infinite: true,
       prevArrow: '<button class="big-slide__slick-arrow big-slide__slick-prev"><span class="lnr lnr-chevron-left big-slide__arrow"></span></button>',
       nextArrow: '<button class="big-slide__slick-arrow big-slide__slick-next"><span class="lnr lnr-chevron-right big-slide__arrow"></span></button>',
     });
 
-    $('.category__inner-block-filter').on('click', function(){
-      $('.category__inner-block-filter-hover').slideToggle();
+    $('.category-product__filter').on('click', function(){
+      $('.category-product__hover').slideToggle();
     });
 
-    var mixer = mixitup('.category__wrapper-block');
+    var mixer = mixitup('.category-product__wrapper');
 
 });
