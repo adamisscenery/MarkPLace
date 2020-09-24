270ef29aa791ec826da67caffaab0b14d893c0fc
adamisscenery <kaurimdu@gmail.com>
2020-09-24 12:23:59 +0600

    validol edited

--- app/js/main.js
+++ app/js/main.js
@@ -13,6 +13,15 @@ $(function(){
       nextArrow: '<button class="big-slide__slick-arrow big-slide__slick-next"><span class="lnr lnr-chevron-right big-slide__arrow"></span></button>',
     });
 
+    $('.slider-product__inner').slick({
+      infinite: true,
+      arrows: true,
+      prevArrow: '<button class="slider-slick-arrow slider-slick-prev"><span class="lnr lnr-chevron-left big-slide__arrow"></span></button>',
+      nextArrow: '<button class="slider-slick-arrow slider-slick-next"><span class="lnr lnr-chevron-right big-slide__arrow"></span></button>',
+      slidesToShow: 3,
+      slidesToScroll: 3
+    });
+
     $('.category-product__filter').on('click', function(){
       $('.category-product__hover').slideToggle();
     });
